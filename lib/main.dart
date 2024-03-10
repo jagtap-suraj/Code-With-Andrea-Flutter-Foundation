82a92a244d3a34d23ab993063ab572a099626240
Suraj Jagtap <suraj552003@gmail.com>
2024-03-10 18:39:26 +0530

    2-2. Navigation with GoRouter

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+
+import 'package:ecommerce_app/src/app.dart';
+import 'package:ecommerce_app/src/localization/string_hardcoded.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
+
+void main() async {
+  // * For more info on error handling, see:
+  // * https://docs.flutter.dev/testing/errors
+  await runZonedGuarded(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    // * Entry point of the app
+
+    // * Remove the # symbol from the URL
+    usePathUrlStrategy();
+    runApp(const MyApp());
+
+    // * This code will present some error UI if any uncaught exception happens
+    FlutterError.onError = (FlutterErrorDetails details) {
+      FlutterError.presentError(details);
+    };
+    ErrorWidget.builder = (FlutterErrorDetails details) {
+      return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Text('An error occurred'.hardcoded),
+        ),
+        body: Center(child: Text(details.toString())),
+      );
+    };
+  }, (Object error, StackTrace stack) {
+    // * Log any errors to console
+    debugPrint(error.toString());
+  });
+}
---
+++ lib/src/routing/app_router.dart
@@ -0,0 +1,110 @@
+import 'package:ecommerce_app/src/features/not_found/not_found_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../features/account/account_screen.dart';
+import '../features/checkout/checkout_screen.dart';
+import '../features/leave_review_page/leave_review_screen.dart';
+import '../features/orders_list/orders_list_screen.dart';
+import '../features/product_page/product_screen.dart';
+import '../features/products_list/products_list_screen.dart';
+import '../features/shopping_cart/shopping_cart_screen.dart';
+import '../features/sign_in/email_password_sign_in_screen.dart';
+import '../features/sign_in/email_password_sign_in_state.dart';
+
+enum AppRoute {
+  home,
+  product,
+  leaveReview,
+  cart,
+  checkout,
+  orders,
+  account,
+  signIn,
+}
+
+final goRoute = GoRouter(
+  initialLocation: '/',
+  debugLogDiagnostics: true,
+  routes: [
+    GoRoute(
+      path: '/',
+      name: AppRoute.home.name,
+      builder: (context, state) => const ProductsListScreen(),
+      routes: [
+        GoRoute(
+          path: 'product/:id',
+          name: AppRoute.product.name,
+          builder: (context, state) {
+            final productId = state.pathParameters['id']!;
+            return ProductScreen(productId: productId);
+          },
+          routes: [
+            GoRoute(
+              path: 'review',
+              name: AppRoute.leaveReview.name,
+              pageBuilder: (context, state) {
+                final productId = state.pathParameters['id']!;
+                return MaterialPage(
+                  key: state.pageKey,
+                  fullscreenDialog: true,
+                  child: LeaveReviewScreen(productId: productId),
+                );
+              },
+            ),
+          ]
+        ),
+        GoRoute(
+          path: 'cart',
+          name: AppRoute.cart.name,
+          pageBuilder: (context, state) => MaterialPage(
+            key: state.pageKey,
+            fullscreenDialog: true,
+            child: const ShoppingCartScreen(),
+          ),
+          routes: [
+            GoRoute(
+              path: 'checkout',
+              name: AppRoute.checkout.name,
+              pageBuilder: (context, state) => MaterialPage(
+                key: state.pageKey,
+                fullscreenDialog: true,
+                child: const CheckoutScreen(),
+              ),
+            ),
+          ],
+        ),
+        GoRoute(
+          path: 'orders',
+          name: AppRoute.orders.name,
+          pageBuilder: (context, state) => MaterialPage(
+            key: state.pageKey,
+            fullscreenDialog: true,
+            child: const OrdersListScreen(),
+          ),
+        ),
+        GoRoute(
+          path: 'account',
+          name: AppRoute.account.name,
+          pageBuilder: (context, state) => MaterialPage(
+            key: state.pageKey,
+            fullscreenDialog: true,
+            child: const AccountScreen(),
+          ),
+        ),
+        GoRoute(
+          path: 'signIn',
+          name: AppRoute.signIn.name,
+          pageBuilder: (context, state) => MaterialPage(
+            key: state.pageKey,
+            fullscreenDialog: true,
+            child: const EmailPasswordSignInScreen(
+              formType: EmailPasswordSignInFormType.signIn,
+            ),
+          ),
+        ),
+      ],
+    ),
+  ],
+  errorBuilder: (context, state) => const NotFoundScreen(),
+);
